1604f2b578e5733a35c1be5c99ae289c4c0e8d58
Jonas Vistrup <jonas.vistrup@gmail.com>
2022-11-28 15:20:33 +0100

    Fixed toString test

--- src/test/java/HypotheticalReasonerTest.java
+++ src/test/java/HypotheticalReasonerTest.java
@@ -50,7 +50,7 @@ class HypotheticalReasonerTest {
             assertEquals("[{},{Q(0),Q(1)},{R(T)}]", S2.get(0).toString(h.getQuery()));
             assertEquals("[{(T/1)},{Q(0),Q(1),R(1)},{}]", S2.get(1).toString(h.getQuery()));
         }
-        assertEquals("Program:\nP(T)<-Q(0),Q(1),R(T)\n\nHypothetical Answers:\n\t[{},{},{Q(0),Q(1),R(T)}]\n\nEvidence Answers:\n\t[{(T/1)},{Q(0),Q(1),R(1)},{}]\n\t[{},{Q(0),Q(1)},{R(T)}]\n\nAnswers:\n\t{(T/1)}\n", h.toString());
+        assertEquals("Program:\nP(T)<-Q(0),Q(1),R(T)\n\nPreprocessing Answers:\n\t[{},{},{Q(0),Q(1),R(T)}]\n\nSupported Answers:\n\t[{(T/1)},{Q(0),Q(1),R(1)},{}]\n\t[{},{Q(0),Q(1)},{R(T)}]\n\nAnswers:\n\t{(T/1)}\n", h.toString());
 
     }
 
@@ -76,7 +76,7 @@ class HypotheticalReasonerTest {
         assertEquals("[{(T/1)},{Q(0),R(1)},{Q(2)}]", S1.get(0).toString(h.getQuery()));
         assertEquals(2, S2.size());
         assertEquals("[{(T/1)},{Q(0),R(1),Q(2)},{}]", S2.get(0).toString(h.getQuery()));
-        assertEquals("Program:\nP(T)<-Q(0),Q(2),R(T)\n\nHypothetical Answers:\n\t[{},{},{Q(0),Q(2),R(T)}]\n\nEvidence Answers:\n\t[{(T/1)},{Q(0),R(1),Q(2)},{}]\n\t[{},{Q(0),Q(2)},{R(T)}]\n\nAnswers:\n\t{(T/1)}\n", h.toString());
+        assertEquals("Program:\nP(T)<-Q(0),Q(2),R(T)\n\nPreprocessing Answers:\n\t[{},{},{Q(0),Q(2),R(T)}]\n\nSupported Answers:\n\t[{(T/1)},{Q(0),R(1),Q(2)},{}]\n\t[{},{Q(0),Q(2)},{R(T)}]\n\nAnswers:\n\t{(T/1)}\n", h.toString());
 
     }
 
@@ -93,10 +93,10 @@ class HypotheticalReasonerTest {
         assertEquals(pString, h.toString());
 
         h.query("Malf(X,T)");
-        assertEquals(pString + "\nHypothetical Answers:\n\t[{},{},{Temp(X,high,T),Temp(X,high,T+1),Temp(X,high,T+2)}]\n\n" + "Evidence Answers:\n\n" + "Answers:\n", h.toString());
+        assertEquals(pString + "\nPreprocessing Answers:\n\t[{},{},{Temp(X,high,T),Temp(X,high,T+1),Temp(X,high,T+2)}]\n\n" + "Supported Answers:\n\n" + "Answers:\n", h.toString());
 
         h.nextTime("Temp(wt25,high, 0)");
-        assertEquals(pString + "\nHypothetical Answers:\n\t[{},{},{Temp(X,high,T),Temp(X,high,T+1),Temp(X,high,T+2)}]\n\n" + "Evidence Answers:\n\t[{(X/wt25),(T/0)},{Temp(wt25,high,0)},{Temp(wt25,high,1),Temp(wt25,high,2)}]\n\n" + "Answers:\n", h.toString());
+        assertEquals(pString + "\nPreprocessing Answers:\n\t[{},{},{Temp(X,high,T),Temp(X,high,T+1),Temp(X,high,T+2)}]\n\n" + "Supported Answers:\n\t[{(X/wt25),(T/0)},{Temp(wt25,high,0)},{Temp(wt25,high,1),Temp(wt25,high,2)}]\n\n" + "Answers:\n", h.toString());
 
 
     }
